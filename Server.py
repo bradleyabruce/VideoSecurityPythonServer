32db3c823c5abeeba69c084ec2d2cf6a2cc411f8
bruceba <bruceba@mail.uc.edu>
2020-05-21 19:02:16 -0400

    Massive Code Cleanup

--- Server.py
+++ Server.py
@@ -2,45 +2,32 @@ import datetime
 from queue import Queue
 import os
 from vidgear.gears import NetGear
-from SingleMotionDetector import SingleMotionDetector
+from BL.CameraInfo import CameraInfo
+from MachineLearning.SingleMotionDetector import SingleMotionDetector
 import cv2
-import VideoBuilder
-import VideoCombiner
-import timedelta
+from VideoHelpers import VideoBuilder, VideoCombiner
 
 
 class Server:
     def __init__(self):
         # Determine debug mode
         self.IS_DEBUG = False
-
-        # Initialize Camera variables
-        self.frame_rate_calc = 1
-        self.freq = cv2.getTickFrequency()
-        self.font = cv2.FONT_HERSHEY_SIMPLEX
-
-        # Initialize the client that we will be connecting to
-        self.client = NetGear(address='192.168.1.30', port='8089', receive_mode=True, protocol='tcp')
-
-        # Initialize ml variables
-        self.frames_before_detection = 15
-        self.frame_count = 0
+        self.camera_info = CameraInfo()
+        self.client = NetGear(address='192.168.1.34', port='8089', receive_mode=True, protocol='tcp')
         self.md = SingleMotionDetector(accumWeight=0.1)
 
         # Directory Variables
-        self.current_directory = None
+        self.day_directory = None
 
         # Time Variables
         self.start_minute = None
         self.minutes_saved = []
-        self.image_array = []
-        self.video_combiner_started = False
 
     # Machine Learning Method
     def detect_motion(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (7, 7), 0)
-        if self.frame_count > self.frames_before_detection:
+        if self.camera_info.frame_count > self.camera_info.frames_before_detection:
             # detect motion in the image
             motion = self.md.detect(gray)
             # check to see if motion was found in the frame
@@ -58,15 +45,19 @@ class Server:
     def draw_camera_info(self, image):
         # grab the current timestamp and draw it on the frame
         timestamp = datetime.datetime.now()
-        cv2.putText(image, timestamp.strftime("%A %d %B %Y %I:%M:%S%p"), (8, 25), self.font, .7, (0, 0, 0), 2,
+        cv2.putText(image, timestamp.strftime("%A %d %B %Y %I:%M:%S%p"), (8, 25), self.camera_info.font, .7, (0, 0, 0),
+                    2,
                     cv2.LINE_AA)
-        cv2.putText(image, timestamp.strftime("%A %d %B %Y %I:%M:%S%p"), (8, 25), self.font, .7, (255, 255, 255), 1,
+        cv2.putText(image, timestamp.strftime("%A %d %B %Y %I:%M:%S%p"), (8, 25), self.camera_info.font, .7,
+                    (255, 255, 255), 1,
                     cv2.LINE_AA)
 
         # Show frame rate
-        cv2.putText(image, "FPS: {0:.2f}".format(self.frame_rate_calc), (8, 55), self.font, .7, (0, 0, 0), 2,
+        cv2.putText(image, "FPS: {0:.2f}".format(self.camera_info.frame_rate_calc), (8, 55), self.camera_info.font, .7, (0, 0, 0),
+                    2,
                     cv2.LINE_AA)
-        cv2.putText(image, "FPS: {0:.2f}".format(self.frame_rate_calc), (8, 55), self.font, .7, (255, 255, 255), 1,
+        cv2.putText(image, "FPS: {0:.2f}".format(self.camera_info.frame_rate_calc), (8, 55), self.camera_info.font, .7,
+                    (255, 255, 255), 1,
                     cv2.LINE_AA)
         return image
 
@@ -74,58 +65,69 @@ class Server:
         directory_name = "Videos/" + datetime.datetime.now().strftime("%m %d %Y")
         if not os.path.exists(directory_name):
             os.makedirs(directory_name)
-        self.current_directory = directory_name
+        self.day_directory = directory_name
 
-    def server_run(self):
+    @staticmethod
+    def get_minute():
+        return datetime.datetime.now().minute
+
+    def combine_videos_if_needed(self, current_minute):
+        if current_minute == 0 and self.day_directory is not None:
+            combiner_queue = Queue()
+            video_combiner = VideoCombiner.VideoCombiner(combiner_queue)
+            video_combiner.daemon = True
+            video_combiner.start()
+            combiner_queue.put((self.day_directory, current_minute))
+
+    def clear_minutes_saved_if_needed(self, current_minute):
+        if current_minute == self.start_minute and (len(self.minutes_saved) > 2):
+            print("Clearing saved minutes with start minute " + str(self.start_minute) + " at current minute: " + str(
+                current_minute))
+            self.minutes_saved.clear()
+
+    def create_video_if_needed(self, current_minute):
+        if current_minute not in self.minutes_saved and current_minute % 5 == 0:
+            # Create new video
+            if self.camera_info.frame_count != 0:
+                builder_queue = Queue()
+                video_builder = VideoBuilder.VideoBuilder(builder_queue)
+                video_builder.daemon = True
+                video_builder.start()
+                builder_queue.put((self.camera_info.image_array.copy(), self.day_directory))
+
+                self.camera_info.image_array.clear()
+                self.minutes_saved.append(current_minute)
+
+    def run(self):
         # Initialize start time
-        self.start_minute = datetime.datetime.now().minute
+        self.start_minute = self.get_minute()
 
         # Continue to retrieve frames from the client as long as it is broadcasting
         while True:
             # Check time
             current_minute = datetime.datetime.now().minute
 
-            # Before getting the new directory, we need to check to see if we need to combine previous videos
-            if current_minute == 40 and self.video_combiner_started is False and self.current_directory is not None:
-                self.video_combiner_started = True
-                combinerQueue = Queue()
-                videoCombiner = VideoCombiner.VideoCombiner(combinerQueue)
-                videoCombiner.daemon = True
-                videoCombiner.start()
-                combinerQueue.put((self.current_directory, current_minute))
+            # self.combine_videos_if_needed(current_minute)
 
-            # Build new directory
+            # This will only create directory every night at midnight
             self.build_current_day_directory()
 
             # Allow for infinite loop of creating videos
-            if current_minute == self.start_minute and (len(self.minutes_saved) > 2):
-                print(
-                    "Clearing saved minutes with start minute " + str(self.start_minute) + " at current minute: " + str(
-                        current_minute))
-                self.minutes_saved.clear()
-                # We need to stop a video from being created if we start at a 5 minute interval of time
-            if current_minute % 5 == 0 and self.frame_count == 0:
-                self.minutes_saved.append(current_minute)
-            # Every five minutes, create a video for us
-            if current_minute not in self.minutes_saved and current_minute % 5 == 0:
-                # Create new video
-                if self.frame_count != 0:
-                    queue = Queue()
-                    videoBuilder = VideoBuilder.VideoBuilder(queue)
-                    videoBuilder.daemon = True
-                    videoBuilder.start()
-                    queue.put((self.image_array.copy(), self.current_directory))
+            self.clear_minutes_saved_if_needed(current_minute)
 
-                    self.image_array.clear()
-                    self.minutes_saved.append(current_minute)
+            # We need to stop a video from being created if we start at a 5 minute interval of time
+            if current_minute % 5 == 0 and self.camera_info.frame_count == 0:
+                self.minutes_saved.append(current_minute)
+
+            # Every five minutes, create a video for us
+            self.create_video_if_needed(current_minute)
 
             t1 = cv2.getTickCount()
-
             # Get frame from client
             frame = self.client.recv()
-            self.frame_count += 1
+            self.camera_info.frame_count += 1
 
-            if self.frame_count == 1:
+            if self.camera_info.frame_count == 1:
                 print("Stream Connected.")
 
             if frame is None:
@@ -136,17 +138,17 @@ class Server:
                 frame = self.draw_camera_info(frame)
 
                 # Save image for video
-                self.image_array.append(frame)
+                self.camera_info.image_array.append(frame)
 
                 # Show frame (for testing purposes)
                 if self.IS_DEBUG:
-                    imS = cv2.resize(frame, (960, 540))  # Resize image
-                    cv2.imshow("output", imS)
+                    ims = cv2.resize(frame, (960, 540))  # Resize image
+                    cv2.imshow("output", ims)
 
                 # FPS Calculation
                 t2 = cv2.getTickCount()
-                time1 = (t2 - t1) / self.freq
-                self.frame_rate_calc = 1 / time1
+                time1 = (t2 - t1) / self.camera_info.freq
+                self.camera_info.frame_rate_calc = 1 / time1
 
             if cv2.waitKey(1) == ord('q'):
                 break
@@ -157,4 +159,4 @@ class Server:
 
 
 server = Server()
-server.server_run()
+server.run()
---
+++ VideoHelpers/VideoBuilder.py
@@ -0,0 +1,82 @@
+import os
+from datetime import datetime, timedelta
+from threading import Thread
+from time import time
+import cv2
+import gc
+
+
+def create_hour_directory(day_directory, video_hour):
+    full_directory = os.getcwd() + "/" + day_directory + "/" + str(video_hour)
+    if not os.path.exists(full_directory):
+        os.makedirs(full_directory)
+    return full_directory
+
+
+def get_hour(timestamp):
+    if timestamp.minute == 0:
+        return (timestamp + timedelta(hours=-1)).hour
+    else:
+        return timestamp.hour
+
+
+def get_file_name(timestamp):
+    if timestamp.minute != 0:
+        video_start_minute = timestamp.minute - 5
+        video_end_minute = timestamp.minute
+        return str(video_start_minute) + "-" + str(video_end_minute) + ".mp4"
+    else:
+        return "55-0.mp4"
+
+
+def build_video(image_array, day_directory):
+    # Time stamp info
+    timestamp = datetime.now()
+
+    # We need to put the videos in the correct directory based on hour
+    video_hour = get_hour(timestamp)
+    full_directory = create_hour_directory(day_directory, video_hour)
+
+    print("-------------------------------------------")
+    print("Generating video for " + timestamp.strftime("%b %d %Y %H %M"))
+    print("Total frames to splice: " + str(len(image_array) - 1))
+
+    # Stopwatch
+    start = time()
+
+    # image properties
+    img = image_array[0]
+    height, width, layers = img.shape
+    size = (width, height)
+
+    file_name = get_file_name(timestamp)
+
+    out = cv2.VideoWriter(full_directory + "/" + file_name, cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+    for i in range(len(image_array)):
+        out.write(image_array[i])
+    out.release()
+
+    # Clear unneeded variables
+    image_array.clear()
+    # Stop stopwatch
+    end = time()
+
+    print("Video complete")
+    print("Elapsed Time: " + str(end - start))
+    print("-------------------------------------------")
+    gc.collect()
+
+
+class VideoBuilder(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            image_array, day_directory = self.queue.get()
+            try:
+                build_video(image_array, day_directory)
+            finally:
+                self.queue.task_done()
+
